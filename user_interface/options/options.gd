23c3848709dbe69583803c443c98a77accb29035
AlexGarbus <alex.garbus@outlook.com>
2025-05-30 23:28:51 -0400

    Quit button

--- user_interface/options/options.gd
+++ user_interface/options/options.gd
@@ -1,6 +1,14 @@
 extends Control
 
 
+@onready var quit_button := %QuitButton
+
+
+func _ready() -> void:
+	if OS.get_name() == "Web":
+		quit_button.hide()
+
+
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -10,3 +18,7 @@ func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
 
 func _on_back_button_pressed() -> void:
 	hide()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
